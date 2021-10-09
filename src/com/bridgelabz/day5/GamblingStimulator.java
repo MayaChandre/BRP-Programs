9dbf64271cf2d247c6dab6014edc17bfc6ae33e8
mayavathi <mayachandre27@gmail.com>
2021-10-08 23:35:53 -0400

    Added:Programs for day 6

--- src/com/bridgelabz/day5/GamblingStimulator.java
+++ src/com/bridgelabz/day5/GamblingStimulator.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class GamblingStimulator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter how many stake u have?");
