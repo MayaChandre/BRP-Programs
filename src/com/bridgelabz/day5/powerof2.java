ad710d98efcbdc0940745e9c01066838b91680ad
mayavathi <mayachandre27@gmail.com>
2021-10-08 23:26:00 -0400

    Added:Programs for day 5

---
+++ src/com/bridgelabz/day5/powerof2.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.day5;
+
+import java.util.Scanner;
+
+public class powerof2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = sc.nextInt();
+		for (int i = 0; i <= n; i++) {
+			System.out.println(i + "\t" + ((int) (Math.pow(2, i))));
+		}
+	}
+}
